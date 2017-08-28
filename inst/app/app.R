93c079efb8a4c92a155dfa670e1871e2fd6efb19
baptiste <baptiste.auguie@gmail.com>
2017-08-28 17:31:03 +1200

    dwd

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(glue)
 # library(headr)
 
 source("selfcontained.R")
@@ -6,21 +7,22 @@ source("selfcontained.R")
 meta <- readLines("_lotr.yaml")
 default <- glue::collapse(meta, sep="\n")
 
+
 shinyApp(
   #UI
-  ui = fluidPage(
-    fluidRow(
-      column(6,
+  ui = fluidPage(fluidRow(
+      column(8,
              textAreaInput("yaml", "Input metadata", value = default, 
-                           width = '200%', rows = length(meta), resize = "both")),
-      column(6, selectInput("tpl", label = h5("TeX fragment"), 
+                           cols=80, rows = length(meta), resize = "vertical")),
+      column(4, selectInput("tpl", label = h5("TeX fragment"), 
                             choices = list("acs" = 1, 
                                            "aps" = 2, 
                                            "osa" = 3, 
                                            "article" = 4), selected = 4),
-             verbatimTextOutput("tex"))
-    )
-  ),
+             verbatimTextOutput("tex"),
+             downloadButton('downloadFragment', 'Download fragment'))
+    
+  )),
   #Server
   server = function(input, output) {
     
@@ -32,6 +34,18 @@ shinyApp(
       
       s
     })
+    
+    output$downloadFragment <- downloadHandler(
+      filename = function() { paste0("frag_", c("acs","aps","osa","article")[as.numeric(input$tpl)],'.tex') },
+      content = function(file) {
+        
+        meta <- yaml::yaml.load(string = input$yaml)
+        
+        tpl <- match.fun(paste0("tpl_", c("acs","aps","osa","article")[as.numeric(input$tpl)]))
+        s <- glue::collapse(purrr::invoke_map_chr(tpl(), meta=meta), sep = "\n%\n\n")
+        writeLines(s, file)
+      }
+    )
     
   }
 )
